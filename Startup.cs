5a1967866573ef129df3fd165f03ec98a371f912
AhmedAbdElmaksood <Ahmed.AbdElmaksood@dafholding.com>
2021-12-16 11:09:26 +0200

    second

--- Startup.cs
+++ Startup.cs
@@ -33,7 +33,7 @@ namespace CommandsService
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "CommandsService", Version = "v1" });
             });
 
-            Console.WriteLine("This is CommandService");
+            Console.WriteLine("This is CommandService updated");
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -41,10 +41,10 @@ namespace CommandsService
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CommandsService v1"));
             }
+            app.UseDeveloperExceptionPage();
+            app.UseSwagger();
+            app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CommandsService v1"));
 
             app.UseHttpsRedirection();
 
